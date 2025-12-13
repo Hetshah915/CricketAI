510eba8fb6ffcd02f9acbe087ec28f6357ce0ee3
Het Shah <hetshah915@gmail.com>
2025-12-13 13:10:43 +0530

    day 1: Initial streamlit setup

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+
+st.set_page_config(page_title="CRICKETAI", layout="wide")
+st.title("CRICKETAI")
+st.subheader("Your Intelligent Cricket Analysis Platform")
+
+st.write("This project would answer your cricket queries ,helps you analyse your weakness and provde tips for performance and also provide visual analysis of your favourite cricketer")
